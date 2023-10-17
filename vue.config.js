a15af4436d7b9b1442611ddc0604978d04a61f29
jose-TAF <118749306+jose-TAF@users.noreply.github.com>
2023-10-16 21:53:28 -0400

    Workflow change

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-// vue.config.js
-module.exports = {
-  publicPath: "https://jlvgit.github.io/remix-scoring",
-};
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/remix-scoring/" : "/",
+});
